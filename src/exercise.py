384fe8cd03e5b7c18d3925e34d75e28443d65e53
Justin Turner <jmturner638@gmail.com>
2021-07-29 04:32:05 -0500

    First Try

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,15 @@
 def main():
     #write your code below this line
+    neg_count = 0
+
+    while True:
+        number = int(input("Give a number:"))
+
+        if number == 0:
+            break
+
+        if number < 0:
+            neg_count += 1
 
 if __name__ == '__main__':
     main()
